e213091374a30a5ce34642e13aaf10f74d600f39
Raphael Kvasne <110569560+rkvasne@users.noreply.github.com>
2025-12-01 23:36:01 -0400

    feat: Adicionar favicon e logo ao projeto
    - Adicionar referências ao favicon.ico em todos os HTMLs (index, landing, consulta, sobre, faq)
    - Substituir ícone SVG do header por logo.png no components.js
    - Garantir consistência visual em todas as páginas
    - Melhorar identidade visual do sistema
    
    Arquivos modificados:
    - index.html: Adicionar tags de favicon
    - landing.html: Adicionar tags de favicon
    - consulta.html: Adicionar tags de favicon
    - sobre.html: Adicionar tags de favicon
    - faq.html: Adicionar tags de favicon
    - js/components.js: Substituir SVG por logo.png no header
    - favicon.ico: Arquivo de ícone do site
    - logo.png: Logo do sistema
    
    Versão: 0.0.8

--- js/components.js
+++ js/components.js
@@ -16,11 +16,7 @@ const Components = (() => {
                     <div class="header-content">
                         <div class="system-brand">
                             <div class="brand-icon">
-                                <svg width="32" height="32" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3">
-                                    </path>
-                                </svg>
+                                <img src="logo.png" alt="Inelegis Logo" width="32" height="32" style="border-radius: 4px;">
                             </div>
                             <div class="brand-text">
                                 <h1>Inelegis</h1>
